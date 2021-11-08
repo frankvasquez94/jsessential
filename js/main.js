67991dd9f28ee2257fec35ae6782074af5b0a6fd
Francisco Vasquez Perez <fvasquezp@lifemiles.loc>
2021-11-08 11:32:32 -0600

    filter

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ var menu = [
 {nombre: "Ceviche", precio: 15.25, pais: "Peru"},
 {nombre: "Tacos", precio: 10.30, pais: "Mexico"},
 {nombre: "Pasta", precio: 11.50, pais: "Italia"},
+{nombre: "Quesadillas", precio: 8.75, pais: "Mexico"},
 ];
 
 var platillo = menu.find(platillo => platillo.nombre == "Ceviche");
@@ -35,3 +36,12 @@ console.log(platillo);
 console.log("===========Find index==================")
 var numPlatillo = menu.findIndex(platillo => platillo.nombre == "Pasta");
 console.log(`El platillo ${menu[numPlatillo].nombre} esta en la posicion ${numPlatillo}`);
+
+
+// Busqueda con Filter
+// find unicamnete devuelve la primera incidencia
+// filter devuelve todos los que cunplen con la condicion
+
+var subMenu = menu.filter(platillo => platillo.pais == "Mexico");
+console.log("========Filter=========");
+console.log(subMenu);
