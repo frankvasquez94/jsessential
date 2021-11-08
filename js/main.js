353f4a2ec637178a10958e8b25056607f5db3499
Francisco Vasquez Perez <fvasquezp@lifemiles.loc>
2021-11-08 10:56:03 -0600

    for each

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 
 "use strict";
 
+
 var platillos = ["Ceviche", "Tacos", "Pasta"];
 
+// For in
+console.log("=====loop for in ======")
 for (let i in platillos) {
     console.log(platillos[i]);
 }
+
+
+// For each
+
+console.log("======For each========")
+// Primero va el valor del elemento y luego el indice
+platillos.forEach((platillo, i) => console.log(i, platillo));
