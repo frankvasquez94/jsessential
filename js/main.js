80efa3716678afff8ae5c7c2760867e70e26bfc3
Francisco Vasquez Perez <fvasquezp@lifemiles.loc>
2021-11-10 14:31:25 -0600

    herencia

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-class Pantalla{
+
+class Producto {
+    constructor(numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    getInfo(){
+        console.log(`Numero de serie: ${this.numSerie}`);
+    }
+}
+
+class Pantalla extends Producto{
 
     // Propiedades
-    constructor(marca, tamanio, modelo){
+    constructor(numSerie, marca, tamanio, modelo){
+        super(numSerie);
         this.marca = marca;
         this.modelo = modelo;
         this.tamanio = tamanio;
@@ -19,12 +31,12 @@ class Pantalla{
 
     // metodos
 
-    info(){
+    static info(){
         console.log(`La pantalla tiene las siguientes caracteristicas, modelo: ${this.modelo}, marca: ${this.marca}, tamanio: ${this.tamanio}`);
     }
 }
 
-var pantalla1 =  new Pantalla("Sansung", "24 plg", "Curvo");
+var pantalla1 =  new Pantalla("12345","Sansung", "24 plg", "Curvo");
 pantalla1.peso = "10kg"
-var pantalla2 =  new Pantalla("HP", "17 plg", "LCD");
+var pantalla2 =  new Pantalla("23456","HP", "17 plg", "LCD");
 pantalla2.peso = "8kg"
