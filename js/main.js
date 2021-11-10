a42e80014786644226683527a8c441fcfd2c8562
Francisco Vasquez Perez <fvasquezp@lifemiles.loc>
2021-11-10 08:40:16 -0600

    trabajando con varias promesas

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
 "use strict";
 
 var contenedor = document.getElementById("contenedor");
+var banderas = document.getElementById("banderas");
 var boton = document.getElementById("boton");
 
 var posts = null;
+var paises = null;
 boton.addEventListener('click', function(){
 
 });
 
 boton.addEventListener('click', function(){
-    fetch("https://jsonplaceholder.typicode.com/posts").then(data => data.json()).then(data => {
+    getPosts().then(data => data.json()).then(data => {
         posts = data;
         mostrarDatos(posts);
+        // Retonamos la promesa de las banderas
+        // Dentro de una promesa
+        return getBanderas();
+    }).then(data => data.json()).then(data => {
+        paises = data;
+        mostrarBanderas(paises);
     });
 });
+
+// Retorna la promesa de posts
+function getPosts(){
+    return fetch("https://jsonplaceholder.typicode.com/posts");
+}
+
+// Retorna la promesa banderas
+function getBanderas(){
+    return fetch("https://restcountries.com/v2/all");
+}
 
 function mostrarDatos(posts)  {
     posts.map((post, i) => {
@@ -27,4 +45,16 @@ function mostrarDatos(posts)  {
         contenedor.appendChild(division);
     });
 
+}
+
+
+function mostrarBanderas(paises) {
+
+    paises.map((pais, i) => {
+        var img = document.createElement("img");
+        img.src = pais.flag
+        img.height = 20;
+        img.width = 40;
+        banderas.appendChild(img);
+    });
 }
