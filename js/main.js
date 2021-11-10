553a5c5f8d3f96a86be9a8ba14b389205c94ac2c
Francisco Vasquez Perez <fvasquezp@lifemiles.loc>
2021-11-10 11:50:02 -0600

    metodos y propiedades

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 class Pantalla{
-constructor(){
 
-}
+    // Propiedades
+    constructor(marca, tamanio, modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tamanio = tamanio;
+    }
+
+    // Agregando mas propiedades
+    set peso(kg){
+        this.kg = kg;
+    }
+
+    get peso(){
+        return this.kg;
+    }
+
+
+    // metodos
+
+    info(){
+        console.log(`La pantalla tiene las siguientes caracteristicas, modelo: ${this.modelo}, marca: ${this.marca}, tamanio: ${this.tamanio}`);
+    }
 }
 
-var pantalla1 =  new Pantalla();
-var pantalla2 =  new Pantalla();
+var pantalla1 =  new Pantalla("Sansung", "24 plg", "Curvo");
+pantalla1.peso = "10kg"
+var pantalla2 =  new Pantalla("HP", "17 plg", "LCD");
+pantalla2.peso = "8kg"
